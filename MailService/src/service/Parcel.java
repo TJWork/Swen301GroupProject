7ff2db4b4aef7a673911172c316558e792947d24
TonyWork <worktony@myvuw.ac.nz>
2013-05-10 10:04:52 +1200

    Parcel constructors corrected

--- MailService/src/service/Parcel.java
+++ MailService/src/service/Parcel.java
@@ -4,14 +4,24 @@ import people.Customer;
 
 
 public class Parcel extends Mail{
+
+
+
+	public Parcel(String day, String toAddress, String from, int priority) {
+		super(day, toAddress, from, priority);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Parcel(String day, String toAddress, String from, int priority,
+			int customerID) {
+		super(day, toAddress, from, priority, customerID);
+		// TODO Auto-generated constructor stub
+	}
+
 	private double weight;
 	
 
-	public Parcel(String day, String toAddress, String id, Customer from,
-			int priority) {
-		super(day, toAddress, id, from, priority);
-		
-	}
 	
 	public double getWeight() {
 		return weight;
